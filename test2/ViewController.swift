964c0f93092002c8191078cd0f2a23238754ca10
bridgespenn <bjp0008@auburn.edu>
2016-11-16 12:12:16 -0600

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Bridges Penn on 11/16/16.
+//  Copyright © 2016 Bridges Penn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
